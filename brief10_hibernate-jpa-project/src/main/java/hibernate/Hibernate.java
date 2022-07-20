b164023f5385359510e0a72413926f0dcfcf7d77
anasmak04 <anasdev8@gmail.com>
2022-07-20 15:57:54 +0200

    not completed 2

--- brief10_hibernate-jpa-project/src/main/java/hibernate/Hibernate.java
+++ brief10_hibernate-jpa-project/src/main/java/hibernate/Hibernate.java
@@ -1,5 +1,50 @@
 package hibernate;
+import java.util.Properties;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import model.Employee;
 public class Hibernate {
+		
+	private static SessionFactory sessionFactory;
 
+	 public static SessionFactory getSessionFactory() {
+	  if (sessionFactory == null) {
+	   try {
+	    Configuration configuration = new Configuration();
+
+	    // Hibernate settings equivalent to hibernate.cfg.xml's properties
+	    Properties settings = new Properties();
+	    settings.put(Environment.DRIVER, "org.postgresql.Driver");
+	    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/Brief10");
+	    settings.put(Environment.USER, "postgres");
+	    settings.put(Environment.PASS, "anas5313N");
+	    settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect\r\n"
+	    		+ "");
+
+	    settings.put(Environment.SHOW_SQL, "true");
+
+	    settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+	    settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+	    configuration.setProperties(settings);
+	    configuration.addAnnotatedClass(Employee.class);
+
+	    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+	      .applySettings(configuration.getProperties()).build();
+	    System.out.println("Hibernate Java Config serviceRegistry created");
+	    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	    return sessionFactory;
+
+	   } catch (Exception e) {
+	    e.printStackTrace();
+	   }
+	  }
+	  return sessionFactory;
+	 }
 }
