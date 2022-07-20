c2d2b5890f043687c8f1ed76457697921555d254
anasmak04 <anasdev8@gmail.com>
2022-07-20 16:11:32 +0200

    not completed 3

---
+++ brief10_hibernate-jpa-project/src/main/java/dao/DaoImpl.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import model.Employee;
+
+public class DaoImpl implements InterfaceDao {
+
+	@Override
+	public void InsertEmp(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void UpdateEmp(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteEmp(String refernce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Employee getEmp(String refernce) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	   	
+}
--- brief10_hibernate-jpa-project/src/main/java/dao/InterfaceDao.java
+++ brief10_hibernate-jpa-project/src/main/java/dao/InterfaceDao.java
@@ -1,5 +1,16 @@
 package dao;
 
-public interface InterfaceDao {
+import java.util.List;
 
+import model.Employee;
+
+public interface InterfaceDao {
+		
+    public void InsertEmp(Employee employee);
+    public void UpdateEmp(Employee employee);
+    public void deleteEmp(String refernce);
+    public Employee getEmp(String refernce);
+    public List < Employee > getAllEmp();
+
+    
 }
